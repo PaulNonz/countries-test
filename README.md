48cd1cef71925e521f5471bb7fdba8c0d195f825
Paul Nonso Ihejirika <89712081+PaulNonz@users.noreply.github.com>
2024-05-31 22:18:03 +0100

    some random complete over all

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # countries-test
 This is just a random API fetch web app that is yet to be complete (30%). I still have the day and night theme button (button-toggler) functionality to complete, and I am yet to link each country's card to it's more info (information) page. 
 Also the filter by region function is still not up and running.
+
+
+30/05/42
+The 
--- js/toggler2.js
+++ js/toggler2.js
@@ -1,39 +1,11 @@
-const buttonTuggler = document.getElementById('navbar-button-toggler');
-const body = document.getElementById('entire-body');
-const navLogo = document.getElementsByTagName('a')[0];
-const navBgCol = document.getElementById('padder');
-const imgCh = document.getElementById('img-sizer');
-const backButton = document.getElementById('back-buttler');
-const backImg = document.getElementById('back-icon');
-
-let changeCol = true;
-
-buttonTuggler.addEventListener('click', function toggle(value) {
-        if (changeCol) {
-                body.style.backgroundColor = '#212E37';
-                buttonTuggler.textContent = 'Light Mode';
-                buttonTuggler.style.color = '#EFF1F0';
-                buttonTuggler.style.backgroundColor = '#2B3743';
-                navLogo.style.color = '#EFF1F0';
-                navBgCol.style.backgroundColor = '#2B3743';
-                backButton.style.color = '#EFF1F0';
-                backButton.style.backgroundColor = '#2B3743';
-                imgCh.src = '/img/2.Moon-_Sleep_-Night.png';
-                backImg.src = '/img/5.left.arrow.night.png';
-        } else {
-                body.style.backgroundColor = '#EFF1F0';
-                buttonTuggler.textContent = 'Dark Mode';
-                buttonTuggler.style.color = 'black';
-                buttonTuggler.style.backgroundColor = '#EFF1F0';
-                navLogo.style.color = 'black';
-                navBgCol.style.backgroundColor = '#EFF1F0';
-                backButton.style.color = 'black';
-                backButton.style.backgroundColor = '#EFF1F0';
-                imgCh.src = '/img/1.Moon-_Sleep_-Day.png';
-                backImg.src = '/img/5.left.arrow.day.png';
-        } 
-        changeCol = !changeCol
-});
-
-
-
+document.getElementById('navbar-button-toggler').addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        document.querySelectorAll('.card-content, .card-content1').forEach(element => {
+            element.classList.toggle('dark-mode-card');
+        });
+    });
+    
+    document.getElementById('backButton').addEventListener('click', () => {
+        window.location.href = 'index.html';
+    });
+    
